efe486e5d8751fb73bd92ae51fe9499120578a10
codeschris <chrismlolwa42@outlook.com>
2023-02-16 08:11:23 -0800

    Creating my first C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char myStr[] = "Programming is like building a multilingual puzzle";
+	char myStr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(myStr);
 	return (0);
