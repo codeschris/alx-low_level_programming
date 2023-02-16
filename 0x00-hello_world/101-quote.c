3875b0fd3c0fdb40d26fb12dab024210b736ea22
codeschris <chrismlolwa42@outlook.com>
2023-02-16 09:58:36 -0800

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,9 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quote, 59);
 
 	return (1);
 }
