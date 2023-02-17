a06b40227deb3ed649aa99b89c17913761832856
codeschris <chrismlolwa42@outlook.com>
2023-02-17 06:33:38 -0800

    Printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+	putchar(alphabet);
+
+	return (0);
+}
