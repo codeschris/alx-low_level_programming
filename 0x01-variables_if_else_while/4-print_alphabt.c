fa18357b1f28dae250042e08b2f81424997b527e
codeschris <chrismlolwa42@outlook.com>
2023-02-17 10:33:41 -0800

    Printing without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	char s;
+
+	for (s = 'a'; s <= 'z'; s++)
+	{
+		if (s == 'e')
+			continue;
+		if (s == 'q')
+			continue;
+
+		putchar(s);
+	}
+
+	return (0);
+}
