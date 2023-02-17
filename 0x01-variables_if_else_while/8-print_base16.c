c31ddfd5cd472007b6424518f2fbe989d0c22fdc
codeschris <chrismlolwa42@outlook.com>
2023-02-17 12:12:49 -0800

    Printing hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	int s;
+
+	for (s = 48; s <= 57; s++)
+		putchar(s);
+	for (s = 65; s <= 70; s++)
+		putchar(s);
+
+	putchar('\n');
+
+	return (0);
+}
