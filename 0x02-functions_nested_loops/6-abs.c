2196ed8998e7cb8ccacba5cd05d7761e0c59f261
codeschris <chrismlolwa42@outlook.com>
2023-02-21 12:06:36 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Check Description
+ *
+ * Description: gives absolute value of given integer
+ *
+ * Return: Nothing
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (i * -1);
+}
