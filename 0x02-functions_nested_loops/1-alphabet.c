3323935ea8190517fbbced09b0f5f4638c838253
codeschris <chrismlolwa42@outlook.com>
2023-02-21 10:04:02 -0800

    Printing alphabets using functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,8 +8,13 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
+	char start;
+
+	for (start = 'a'; start <= 'z'; start++)
+		_putchar(start);
+	_putchar('\n');
+
 	return (0);
 }
