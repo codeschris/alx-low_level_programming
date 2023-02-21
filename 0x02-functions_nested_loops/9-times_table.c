ef5e1fc9507497d1b3eb5e1bb554c776d54d60e7
codeschris <chrismlolwa42@outlook.com>
2023-02-21 12:49:42 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,7 +24,7 @@ void times_table(void)
 					_putchar(' ');
 				_putchar(k + '0');
 
-				if (j == 0)
+				if (j == 9)
 					continue;
 				_putchar(',');
 				_putchar(' ');
@@ -33,7 +33,7 @@ void times_table(void)
 			{
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
-				if (j == 0)
+				if (j == 9)
 					continue;
 				_putchar(',');
 				_putchar(' ');
