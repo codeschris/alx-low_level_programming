2fc5030da8ad1701e8a810c56eb2eec50df72783
codeschris <chrismlolwa42@outlook.com>
2023-02-23 11:44:14 -0800

    print diagonally

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints lines diagonally according to input
+ * @n: input
+ *
+ * Return: Nothing
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
+
