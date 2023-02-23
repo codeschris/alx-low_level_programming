3b97de3dded9352309a2d760286c89e8980a0762
codeschris <chrismlolwa42@outlook.com>
2023-02-23 10:17:00 -0800

    printing 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14 ten times
+ *
+ * Return: Nothing
+ */
+
+void more_numbers(void)
+{
+	int n, c;
+
+	for (n = '0'; n <= '10'; n++)
+	{
+		for (c = '0'; c <= '14'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
