aaf0e1d041296df80425a89aca2d703a7a8ceded
codeschris <chrismlolwa42@outlook.com>
2023-02-23 09:17:57 -0800

    Printing but 2 numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers except two
+ *
+ * Return: Nothing
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c == '2' && c == '4')
+			continue;
+		_putchar(c);
+	}
+	putchar('\n');
+}
