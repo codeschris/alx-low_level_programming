e4707e532e15875c19500a7444cd2158108b0da6
codeschris <chrismlolwa42@outlook.com>
2023-02-23 13:20:23 -0800

    final

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_triangles - functions prints triangles according
+ * print_triangle - functions prints triangles according
  * to input given
  * @size: input
  *
@@ -10,23 +10,23 @@
 
 void print_triangle(int size)
 {
-	int x, y, z;
+	int x, y, z = size - 1;
 
-	if (size <= 0)
-		_putchar('\n');
-	else
+	if (size > 0)
 	{
-		for (x = 1; x < size; x++)
+		for (x = 0; x < size; x++)
 		{
-			for (z = size - x; z > 1; z--)
+			for (y = 0; y < size; y++)
 			{
-				_putchar(' ');
+				if (y < z)
+					_putchar(' ');
+				else
+					_putchar(35);
 			}
-			for (y = 1; y < x; y++)
-			{
-				_putchar('#');
-			}
+			z--;
 			_putchar('\n');
 		}
 	}
+	else
+		_putchar('\n');
 }
