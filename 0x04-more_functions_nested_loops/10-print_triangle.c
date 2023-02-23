a42fc1832e29613b9567037cc8295d2bca1cadcc
codeschris <chrismlolwa42@outlook.com>
2023-02-23 12:35:36 -0800

    triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangles - functions prints triangles according
+ * to input given
+ * @size: input
+ *
+ * Return: Nothing
+ */
+
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (x = 1; x <= size; x++)
+	{
+		for (y = size - x; y >= 1; y--)
+		{
+			for (z = 1; z <= x; z++)
+			{
+				_putchar('\n');
+			}
+			_putchar('\n');
+		}
+	}
+}
