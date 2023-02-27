c2a8fc38070d482efaa6fe62323768eb648bba46
codeschris <chrismlolwa42@outlook.com>
2023-02-27 10:29:32 -0800

    creating print function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - function prints a string, followed by newline
+ * to stdout
+ *
+ * @str: input string
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	char x;
+
+	str = &x;
+	fputs(x, stdout);
+}
