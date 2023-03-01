e390b5f247990fbc0a1b5e0a4127dd801580e5ce
codeschris <chrismlolwa42@outlook.com>
2023-03-01 09:11:22 -0800

    first run

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenates strings 
+ * according to bytes being allocated
+ *
+ * @dest: input string
+ * @src: input string
+ * @n: bytes in number format
+ *
+ * Return: pointer to string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *tmp = dest;
+
+	return (tmp);
+}
