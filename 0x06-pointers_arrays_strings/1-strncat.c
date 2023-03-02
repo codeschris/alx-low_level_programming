560f48cec248f1bbf002749b8d5150382661d5d8
codeschris <chrismlolwa42@outlook.com>
2023-03-01 20:29:32 -0800

    second run

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncat - function that concatenates strings 
+ * _strncat - function that concatenates strings
  * according to bytes being allocated
  *
  * @dest: input string
@@ -13,7 +13,27 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	char *tmp = dest;
+	char *tmp = dest, *st = src;
+	int len = 0, i = 0;
+
+	while (*dest)
+		dest++;
+
+	while (*src)
+	{
+		src++;
+		len++;
+	}
+
+	if (n > len)
+		n = len;
+
+	src = st;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	*dest = '\0';
 
 	return (tmp);
 }
