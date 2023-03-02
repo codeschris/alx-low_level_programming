c505695bdd79100e1d2bc3be2e8109aeb35fa2a4
codeschris <chrismlolwa42@outlook.com>
2023-03-01 23:56:58 -0800

    fifth run

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - function converts lowercase letters
+ * to uppercase letters
+ *
+ * @s : input string
+ *
+ * Return: uppercase letters/strings
+ */
+
+char *string_toupper(char *s)
+{
+	char *st = s;
+
+	while (*s)
+	{
+		if (*s >= 'a' && s <= 'z')
+			*s -= 32;
+		s++
+	}
+
+	return (st);
+}
