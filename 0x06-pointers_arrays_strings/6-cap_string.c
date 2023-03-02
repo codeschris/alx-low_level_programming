7144a9637ff9bfe2edf088f63f35e4c650993f47
codeschris <chrismlolwa42@outlook.com>
2023-03-02 02:53:53 -0800

    fixing

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,30 @@
+#include "main.h"
 
+/**
+ * cap_string - function capitalizes words in string
+ *
+ * @s: input string
+ *
+ * Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] -= 32;
+		}
+		if (s[i] ==  ' ')
+		{
+			++i;
+
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] -= 32;
+		}
+	}
+}
