c40bfdff15ce7b87a2b101afe7efbd26cee13c49
codeschris <chrismlolwa42@outlook.com>
2023-03-02 06:32:15 -0800

    extra

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_number - function prints an integer
+ *
+ * @n: input integer
+ *
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,5 +8,8 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *x);
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char s);
 
 #endif
