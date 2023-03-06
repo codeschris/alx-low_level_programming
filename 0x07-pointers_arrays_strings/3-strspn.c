117a16d84c192071f784d685d24111144119957d
codeschris <chrismlolwa42@outlook.com>
2023-03-06 04:52:17 -0800

    size of segment

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strspn - function returns number of bytes in
+ * initial segment of s
+ *
+ * @s: input string
+ * @accept: input character(s)
+ *
+ * Return: number of bytes in segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int n = 0;
+
+	if ((*s == '\0') || (*accept == '\0'))
+		return (n);
+
+	while (*s && _strchr(accept, *s++))
+		n++;
+
+	return (n);
+}
