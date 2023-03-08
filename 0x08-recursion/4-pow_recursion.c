059a9e713aec6d68b919c223d2ea4bd97b37f407
codeschris <chrismlolwa42@outlook.com>
2023-03-08 04:32:13 -0800

    powers

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function retruns x raised to the
+ * power of y
+ *
+ * @x: input integer
+ * @y: input integer acting as power
+ *
+ * Return: result of the power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (x != 0)
+		return (x * _pow_recursion(x, y - 1));
+	else
+		return (1);
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function returns natural 
+ * square root of a number
+ *
+ * @n: input integer
+ *
+ * Return: sqrt of the input
+ */
+
+int _sqrt_recursion(int n)
+{
+
