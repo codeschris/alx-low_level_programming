9ae0b5ec7496206ec14f195fea702c5f31b1ed97
codeschris <chrismlolwa42@outlook.com>
2023-03-08 03:56:58 -0800

    correction

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,16 +10,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	int n = 0;
-
-	while (s[n] != '\0')
-	{
-		n++;
-	}
-
 	if (*s)
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_print_rev_recursion(s[--n]);
 	}
 }
