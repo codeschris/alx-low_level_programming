63755d4689175b99efd37afdc854a4f982a0ed65
codeschris <chrismlolwa42@outlook.com>
2023-03-08 04:51:01 -0800

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,8 +12,11 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
+	if (y == 0)
+		return (1);
+	else if (y < 0)
 		return (-1);
-	if (x <= 0 && x >= 0)
-		return (x * _pow_recursion(x, y - 1));
+	else if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
 }
