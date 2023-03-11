8440a67e21f911385d9b0b2d1316db79ffdf7e0d
codeschris <chrismlolwa42@outlook.com>
2023-03-11 04:36:33 -0800

    changes

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -13,9 +13,6 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	if (argv[0])
-	{
-		for (i = 0; i < argc; i++)
-			printf("\n%s", argv[i]);
-	}
+	for (i = 0; i < argc; i++)
+		printf("\n%s", argv[i]);
 }
