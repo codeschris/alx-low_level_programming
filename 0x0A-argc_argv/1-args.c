d2a18feda13cbc7bf535f81d8ad4cca7ff6fe451
codeschris <chrismlolwa42@outlook.com>
2023-03-11 00:58:49 -0800

    change

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - function prints no. of arguments passed
  *
  * @argc: number of arguments
  * @argv: array of arguments
@@ -13,5 +13,6 @@ int main(int argc, char *argv[])
 {
 	if (argv[0] != '\0')
 		printf("%d\n", argc - 1);
+
 	return (0);
 }
