909e4d57f86be589a6b05e5a072cf233145d2081
codeschris <chrismlolwa42@outlook.com>
2023-03-14 10:36:08 -0700

    corrections

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,22 +17,24 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = (int **)malloc(sizeof(int *) * height);
+	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
 		return (NULL);
 
-	for (; i < height; i++)
-	{
-		arr[i] = (int *)malloc(sizeof(int) * width);
-		if (arr[i] == NULL)
-			return (NULL);
-	}
-
 	for (i = 0; i < height; i++)
 	{
-		for (; j < width; j++)
+		arr[i] = malloc(sizeof(int *) * width);
+		if (arr[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(arr[j]);
+			}
+			free(arr);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 	}
-
 	return (arr);
 }
