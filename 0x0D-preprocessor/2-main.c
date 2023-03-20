1e34839326b2ac2f0b2d41b8369fa6d4e037bb9a
codeschris <chrismlolwa42@outlook.com>
2023-03-20 00:07:49 -0700

    adding answers to q0 to q4

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program prints name of source file
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
