be5359be7847b94b4f61eb73298e36bfcff3dbbb
codeschris <chrismlolwa42@outlook.com>
2023-04-04 07:14:36 -0700

    displaying no. of elements in the list

---
+++ 0x13-more_singly_linked_lists/1-listlen_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - function returns no. of elements in 
+ * a linked list
+ *
+ * @h: ptr to the list struct
+ *
+ * Return: no. of elements in the linked list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
