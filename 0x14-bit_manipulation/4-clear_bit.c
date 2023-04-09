ae1386524bea2e578f24454e9c31a815bb03f767
codeschris <chrismlolwa42@outlook.com>
2023-04-09 04:23:50 -0700

    addressing size of octate and initial bit before shifting

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,8 +13,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int max = 0x01;
 
-	max = ~(1 << index);
-	if (max == 0x00)
+	max = ~(max << index);
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	*n &= max;
 	return (1);
