d2d3eb56a471f9fc23b91be3e04f5550a11dbf8c
codeschris <chrismlolwa42@outlook.com>
2023-04-09 04:29:57 -0700

    addressing length of index and initial bits

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,7 +14,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int max = 0x01;
 
 	max <<= index;
-	if (max == 0)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	*n |= max;
 	return (1);
