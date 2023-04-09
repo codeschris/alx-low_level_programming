0f4535079e08e94b113684b116d65314dbf6319f
codeschris <chrismlolwa42@outlook.com>
2023-04-09 03:18:26 -0700

    correction in flipping bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,13 +13,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int flip = 0;
 	unsigned long int exor = (n ^ m);
-	unsigned long int max = 0x01;
 
-	while (max <= exor)
+	while (exor)
 	{
-		if (max & exor)
-			flip++;
-		max <<= 1;
+		flip += exor & 1;
+		exor >>= 1;
 	}
 	return (flip);
 }
